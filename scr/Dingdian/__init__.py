3f5cf7dc1b5e879c40f7c9ae17d0b3c0cfa5feff
VeronicaAlexia <Elaina-Alex@proton.me>
2022-08-17 10:28:41 +0800

    ~

--- scr/Dingdian/__init__.py
+++ scr/Dingdian/__init__.py
@@ -8,43 +8,17 @@ class DingdianAPI:
 
     @staticmethod
     def get_chapter_info_by_chapter_id(chapter_url):
-        return etree.HTML(get(api_url='https://www.ddyueshu.com' + chapter_url, retry=5))
-
-
-
-def get_book_info(book_id: str, retry: int = 0) -> [dict, None]:  # get book info from url by book_id
-    response = get(api_url="https://www.ddyueshu.com/" + book_id, retry=retry, gbk=True)
-    if response is not None and isinstance(response, str):  # if the response is not None and is a string
-        html_str = etree.HTML(response)  # parse html string to lxml.etree.ElementTree
-        return book_info_json(
-            book_id=book_id,
-            book_name=html_str.xpath('//*[@id="info"]/h1')[0].text,
-            author_name=html_str.xpath('//*[@id="info"]/p[1]')[0].text.split('：')[-1],
-            chapter_url_list=[url for url in html_str.xpath('//*[@id="list"]/dl/dd[*]/a/@href')],
-            cover_url='https://www.ddxstxt8.com' + html_str.xpath('//div[@id="fmimg"]/img')[0].get('src'),
-        )  # return a dict with book info
-    else:
-        if retry <= 10 and response != 404:
-            return get_book_info(book_id, retry + 1)
-        return print("get book info failed, book_id is {}".format(book_id))
+        return etree.HTML(get(api_url='https://www.ddyueshu.com' + chapter_url, retry=5, gbk=True))
 
 
 def get_chapter_info(chapter_url: str, index: int, content: str = "", retry: int = 0) -> [dict, None]:
-    response = get(api_url="https://www.ddyueshu.com" + chapter_url, retry=retry, gbk=True)
-    if response is not None and isinstance(response, str):
-        for book in response.split('<div id="content">')[1].split("<script>")[0].split("<br />"):
-            if book.strip() == "":
-                continue
-            if '请记住本书首发域名' in book or '书友大本营' in book:
-                continue
-            if book is not None and len(book.strip()) != 0:
-                content += book.strip() + "\n"
-        return chapter_info_json(
-            index=index, url=chapter_url, content=content,
-            title=etree.HTML(str(response)).xpath('//div[@class="bookname"]/h1')[0].text.strip()
-        )  # get chapter info from html string and return a dict with chapter info
-
-    else:
-        if retry <= 10:
-            return get_chapter_info(chapter_url, index, content, retry + 1)
-        return print("get chapter info failed, chapter_url is {}".format(chapter_url))
+    response = DingdianAPI.get_chapter_info_by_chapter_id(chapter_url)
+    title = response.xpath('//div[@class="bookname"]/h1/text()')[0].strip()
+    for book in response.xpath('//*[@id="content"]/text()'):
+        if book.strip() == "":
+            continue
+        if '请记住本书首发域名' in book or '书友大本营' in book:
+            continue
+        if book is not None and len(book.strip()) != 0:
+            content += book.strip() + "\n"
+    return chapter_info_json(index=index, url=chapter_url, content=content, title=title)
--- scr/rule.py
+++ scr/rule.py
@@ -54,3 +54,34 @@ def chapter_json(index: int, url: str, title: str, content: str, image_list: lis
         }
     except Exception as error:
         print("chapter_info_json", error)
+
+
+def book_json(
+        book_id: str = None,
+        book_name: str = None,
+        author_name: str = None,
+        book_tag: str = None,
+        book_intro: str = None,
+        book_status: str = None,
+        cover_url: str = None,
+        book_uptime: str = None,
+        last_chapter_title: str = None,
+        chapter_url_list: list = None,
+        book_words: str = None,
+) -> [dict, None]:
+    try:
+        return {
+            "bookId": book_id,
+            "bookName": book_name,
+            "authorName": author_name,
+            "bookCoverUrl": cover_url,
+            "chapUrl": chapter_url_list,
+            "bookWords": book_words,
+            "bookTag": book_tag,
+            "bookIntro": book_intro,
+            "bookStatus": book_status,
+            "lastChapterTitle": last_chapter_title,
+            "bookUptime": book_uptime
+        }
+    except Exception as error:
+        print("book_info_json", error)
