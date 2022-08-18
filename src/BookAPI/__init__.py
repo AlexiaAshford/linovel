d906b895ae0beba6416fc940940721371c1f578d
VeronicaAlexia <Elaina-Alex@proton.me>
2022-08-18 19:49:52 +0800

    ~

--- src/BookAPI/__init__.py
+++ src/BookAPI/__init__.py
@@ -1,7 +1,16 @@
-import re
 import src
 from lxml import etree
-from tenacity import retry, stop_after_attempt
+import constant
+from tenacity import retry, stop_after_attempt, wait_fixed
+
+
+@retry(stop=stop_after_attempt(7), wait=wait_fixed(0.5))
+def get(api_url: str, gbk: bool = False, params: dict = None, method: str = "GET"):
+    try:
+        return etree.HTML(src.request(method=method, api_url=api_url, gbk=gbk, params=params))
+    except Exception as error:
+        print(error)
+        raise Exception("[error] method:{} api_url: {}".format(method, api_url))
 
 
 class XbookbenAPI:
@@ -11,21 +20,18 @@ class XbookbenAPI:
 
     @staticmethod
     def get_book_info_by_book_id(book_id: str):
-        return etree.HTML(
-            src.request(api_url=XbookbenAPI.xbookben_host + XbookbenAPI.book_info_by_book_id.format(book_id))
-        )
+        return get(api_url=XbookbenAPI.xbookben_host + XbookbenAPI.book_info_by_book_id.format(book_id))
 
     @staticmethod
-    @retry(stop=stop_after_attempt(7))
     def get_chapter_info_by_chapter_id(chapter_url: str):
-        return etree.HTML(src.request(api_url=XbookbenAPI.xbookben_host + chapter_url))
+        return get(api_url=XbookbenAPI.xbookben_host + chapter_url)
 
     @staticmethod
     def get_book_info_by_keyword(keyword: str):
-        response = etree.HTML(src.request(
+        response = get(
             method="POST", params={"searchkey": keyword},
             api_url=XbookbenAPI.xbookben_host + XbookbenAPI.book_info_by_keyword
-        ))
+        )
         return list(zip(
             response.xpath(constant.rule.XbookbenRule.Search.book_name),
             response.xpath(constant.rule.XbookbenRule.Search.book_img),
@@ -40,67 +46,40 @@ class LinovelAPI:
 
     @staticmethod
     def get_book_info_by_book_id(book_id: str):
-        return etree.HTML(src.request(LinovelAPI.linovel_host + LinovelAPI.book_info_by_book_id.format(book_id)))
+        return get(LinovelAPI.linovel_host + LinovelAPI.book_info_by_book_id.format(book_id))
 
     @staticmethod
-    @retry(stop=stop_after_attempt(7))
     def get_chapter_info_by_chapter_id(chapter_url: str):
-        return etree.HTML(src.request(api_url=LinovelAPI.linovel_host + chapter_url))
+        return get(api_url=LinovelAPI.linovel_host + chapter_url)
 
     @staticmethod
     def get_book_info_by_keyword(keyword: str, page: int = 1):
-        params = {'kw': keyword} if page < 2 else {'kw': keyword, 'page': page, 'sort': 'hot', 'target': 'complex',
-                                                   'mio': 1, 'ua': 'Mozilla/5.0'}
-        response = src.request(api_url=LinovelAPI.linovel_host + LinovelAPI.book_info_by_keyword, params=params)
+        params = {'kw': keyword} if page < 2 else {
+            'kw': keyword, 'page': page, 'sort': 'hot', 'target': 'complex', 'mio': 1, 'ua': 'Mozilla/5.0'}
+        response = get(api_url=LinovelAPI.linovel_host + LinovelAPI.book_info_by_keyword, params=params)
         return list(zip(
-            etree.HTML(response).xpath(constant.rule.LinovelRule.Search.book_img),
-            etree.HTML(response).xpath(constant.rule.LinovelRule.Search.book_name)
+            response.xpath(constant.rule.LinovelRule.Search.book_img),
+            response.xpath(constant.rule.LinovelRule.Search.book_name)
         ))
 
 
 class DingdianAPI:
     @staticmethod
     def get_book_info_by_book_id(book_id: str):
-        return etree.HTML(src.request("https://www.ddyueshu.com/{}".format(book_id), gbk=True))
+        return get(api_url="https://www.ddyueshu.com/{}".format(book_id), gbk=True)
 
     @staticmethod
-    @retry(stop=stop_after_attempt(7))
     def get_chapter_info_by_chapter_id(chapter_url):
-        return etree.HTML(src.request(api_url='https://www.ddyueshu.com' + chapter_url, gbk=True))
-
-
-class BiquPavilionAPI:
-    @staticmethod
-    def get_book_info_by_book_id(book_id: str) -> [dict, None]:  # get book info from url by book_id
-        response = src.request("https://infosxs.pigqq.com/BookFiles/Html/{}/info.html".format(book_id))
-        if response is not None and isinstance(response, dict):  # if the response is not None and is a dict
-            return response.get("data")  # get book info from response dict
+        return get(api_url='https://www.ddyueshu.com' + chapter_url, gbk=True)
 
 
 def get_sort(tag_name: str, page: int, retry: int = 0):  # get sort from url by page
     params = {"sort": "words", "sign": "-1", "page": page}
-    response = src.request(api_url="https://www.linovel.net/cat/-1.html", params=params)
-    if isinstance(response, str):
-        sort_html_list = etree.HTML(response).xpath('//a[@class="book-name"]')
-        sort_info_list = [i.get('href').split('/')[-1][:-5] for i in sort_html_list]
-        if sort_info_list and len(sort_info_list) != 0:
-            return sort_info_list
-    else:
-        if retry <= 10:
-            return get_sort(tag_name, page, retry + 1)
-        return print("get sort failed, page is {}".format(page))
-
-
-def search_book(book_name: str) -> [list, None]:
-    response = src.request(api_url="https://www.linovel.net/search/", params={"kw": book_name})
-    if response is not None and isinstance(response, str):
-        html_str = response.split('<div class="rank-book-list">')[1]
-        book_id_list = re.findall(r'<a href="/book/(\d+).html"', str(html_str))
-        if len(book_id_list) != 0:
-            return book_id_list
-        else:
-            return []
-        # print(book_id)
+    response = get(api_url="https://www.linovel.net/cat/-1.html", params=params)
+    sort_html_list = response.xpath('//a[@class="book-name"]')
+    sort_info_list = [i.get('href').split('/')[-1][:-5] for i in sort_html_list]
+    if sort_info_list and len(sort_info_list) != 0:
+        return sort_info_list
 
 
 def get_chapter_cover(html_string: [str, etree.ElementTree]) -> [list, None]:
