5f0eeb9294a385af21b234a8d9d7aa9e047f3edb
VeronicaAlexia <Elaina-Alex@proton.me>
2022-10-05 22:34:22 +0800

    ~

--- src/http_utils/__init__.py
+++ src/http_utils/__init__.py
@@ -7,7 +7,7 @@ from tenacity import *
 
 session = requests.Session()
 try:
-    fake_useragent_list = json.loads(open("./src/fake_useragent_0.1.11.json", "r", encoding="utf-8").read())
+    fake_useragent_list = json.loads(open("./src/http_utils/fake_useragent_0.1.11.json", "r", encoding="utf-8").read())
 except Exception as error:
     print("fake_useragent_list error: {}".format(error))
     fake_useragent_list = json.loads(open("./fake_useragent_0.1.11.json", "r", encoding="utf-8").read())
