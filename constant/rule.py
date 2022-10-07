a10f9ff523728a4943345116fbc25127fd8d739b
VeronicaAlexia <Elaina-Alex@proton.me>
2022-10-07 15:15:08 +0800

    ~

--- constant/rule.py
+++ constant/rule.py
@@ -1,6 +1,23 @@
 from config import *
 
 
+class NovelRule:
+    def __init__(self, novel_rule):
+        self.novel_rule = novel_rule
+        self.book_img = novel_rule['book_img']
+        self.book_name = novel_rule['book_name']
+        self.book_author = novel_rule['book_author']
+        self.chapter_url_list = novel_rule['chapter_url_list']
+        self.book_state = novel_rule['book_state']
+        self.book_label = novel_rule['book_label']
+        self.book_intro = novel_rule['book_intro']
+        self.last_chapter_title = novel_rule['last_chapter_title']
+        self.book_words = novel_rule['book_words']
+        self.book_update_time = novel_rule['book_update_time']
+        self.chapter_title = novel_rule['chapter_title']
+        self.chapter_content = novel_rule['chapter_content']
+
+
 class WebRule:
     class XbookbenRule:
         book_img = '/html/body/div/div[2]/div/div/div[1]/span/img/@src'
