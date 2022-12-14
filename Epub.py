ea2990cc0d02bce84765ad104b82740c455b5ec8
VeronicaAlexia <Elaina-Alex@proton.me>
2022-12-14 17:03:31 +0800

    ~

--- Epub.py
+++ Epub.py
@@ -10,6 +10,7 @@ class EpubHtml:
         self.html_template = epub.EpubHtml
 
     def set_description(self):
+        Vars.current_book.book_words = re.sub(r'\s|\n|\r|字数：', '', Vars.current_book.book_words)
         self.description = "<html><head></head><body>"
         if Vars.current_book.book_cover is not None:
             self.description += f'<img src="cover.png" alt="{Vars.current_book.bookName} 封面">'
@@ -19,10 +20,10 @@ class EpubHtml:
             self.description += '<h2>书籍作者:{}</h2>\n'.format(Vars.current_book.book_author)
         if Vars.current_book.book_id is not None:
             self.description += '<h3>书籍序号:{}</h3>'.format(Vars.current_book.book_id)
-        if Vars.current_book.book_status is not None:
+        if re.sub(r'\s|\n|\r', '', Vars.current_book.book_status) != "":
             self.description += '<h4>书籍状态:{}</h4>\n'.format(Vars.current_book.book_status)
-        if Vars.current_book.book_words is not None:
-            self.description += '<h4>字数信息:</h4>{}\n'.format(Vars.current_book.book_words.replace("字数：", ""))
+        if Vars.current_book.book_words != "":
+            self.description += '<h4>字数信息:{}</h4>\n'.format(Vars.current_book.book_words)
         if Vars.current_book.last_chapter_title is not None:
             self.description += '<h4>最新章节:{}</h4>\n'.format(Vars.current_book.last_chapter_title)
         if Vars.current_book.book_tag is not None:
