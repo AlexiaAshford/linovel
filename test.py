6d7ba0eaf2b323cf9a188b663e72b81b6bb66e4b
VeronicaAlexia <Elaina-Alex@proton.me>
2022-12-14 12:18:11 +0800

    ~

---
+++ test.py
@@ -0,0 +1,46 @@
+import book
+from src import *
+from config import *
+
+
+def init_config_book_source():
+    import json
+    import constant
+    book_source_path = "./book_source/{}.json".format(Vars.current_book_type.split(".")[-2])
+    if not os.path.exists(book_source_path):
+        return False
+    # print(book_source_path)
+    Vars.current_book_source = json.loads(open(book_source_path, "r", encoding="utf-8").read())
+    print(Vars.current_book_source)
+    Vars.current_book_api = API.Response
+    Vars.current_book_rule = constant.NovelRule()
+    print("下载源已设置为: {}".format(Vars.current_book_type))
+    return True
+
+
+if __name__ == '__main__':
+    update_config()
+    book_id = "6072"
+    Vars.current_book_classify_name = "tongren"
+    Vars.current_book_type = "http://www.trxs.cc"
+    Vars.current_book_gbk = True
+    if not init_config_book_source():
+        raise Exception("book source not found, please check your book type, book type:", Vars.current_book_type)
+
+    book_info_html = Vars.current_book_api.get_book_info_by_book_id(book_id)
+    print("=== book_info_html ===")
+    if book_info_html is None:
+        print("<error>", "[red]Get book info error, please check your book id.[/red]")
+        exit(1)
+    init_book_info_template(book_info_html)
+    init_chapter_url_list(book_info_html)
+    print("=== init_chapter_url_list ===")
+    if Vars.current_book.get("chapter_url_list") is None:
+        print("<error>", "[red]The chapter list is empty, please check your book id.[/red]")
+        exit(1)
+    if Vars.current_book and Vars.current_book.get("bookName") is not None:
+        Vars.current_book = book.BookConfig(Vars.current_book)
+        Vars.current_book.init_content_config()
+        Vars.current_book.multi_thread_download_book()
+    else:
+        print("<error>", "[red]Download book error,please check your input app type name.[/red]")
--- update_source.py
+++ update_source.py
@@ -1,14 +1,17 @@
 import json
 import os
 
-book_source_list = os.listdir("book_source")
-print(book_source_list)
-
-for book_source in book_source_list:
-    with open("book_source/" + book_source, "r", encoding="utf-8") as f:
-        file = json.loads((f.read()))
-        if not file.get("json_page"):
-            file["json_page"] = False
-
-        with open("book_source/" + book_source, "w", encoding="utf-8") as w:
-            w.write(json.dumps(file, ensure_ascii=False, indent=4))
+if __name__ == '__main__':
+    # read book source file name
+    book_source_list = os.listdir("book_source")
+    # print(book_source_list)
+    print("book_source_list length: ", len(book_source_list))
+    for book_source in book_source_list:
+        # read book source file content
+        with open("book_source/" + book_source, "r", encoding="utf-8") as f:
+            file = json.loads((f.read()))
+            if not file.get("json_page"):
+                file["json_page"] = False  # add json_page key
+            # write book source file for update
+            with open("book_source/" + book_source, "w", encoding="utf-8") as w:
+                w.write(json.dumps(file, ensure_ascii=False, indent=4))
